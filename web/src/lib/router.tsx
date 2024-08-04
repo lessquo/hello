768f378c5a5fb125e6eb8ee4fa9a977c61226b3a
lessquo <lessquo@gmail.com>
2024-08-04 17:35:07 +0900

    Rename Login to SignIn

--- web/src/lib/router.tsx
+++ web/src/lib/router.tsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../routes/Home';
-import { Login } from '../routes/Login';
 import { Root } from '../routes/Root';
+import { SignIn } from '../routes/Signin';
 
 export const router = createBrowserRouter([
   {
     Component: Root,
     children: [
       { index: true, Component: Home },
-      { path: '/login', Component: Login },
+      { path: '/sign-in', Component: SignIn },
     ],
   },
 ]);
